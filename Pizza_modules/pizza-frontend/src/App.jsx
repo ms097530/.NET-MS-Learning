9b7d21f4bbea83b72f232429c75f9ed3e4f0f5cf
Michael Schultz <mikewschultz9029@gmail.com>
2023-06-09 17:39:21 -0400

    renamed folders and initialized frontend with vite

---
+++ Pizza_modules/pizza-frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Pizza from './Pizza'
+
+function App()
+{
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Pizza />
+    </>
+  )
+}
+
+export default App
