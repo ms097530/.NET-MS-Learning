be1f21301e30e330eaf60e6909fa24186386d882
Michael Schultz <mikewschultz9029@gmail.com>
2023-06-09 17:39:45 -0400

    added variable

--- Pizza_modules/pizza-frontend/src/Pizza.jsx
+++ Pizza_modules/pizza-frontend/src/Pizza.jsx
@@ -1,5 +1,12 @@
 import React, { useState } from 'react'
 
+let pizzas = [{
+    id: 1, name: 'Cheese pizza', description: 'very cheesy'
+},
+{
+    id: 2, name: 'Al Tono pizza', description: 'lots of tuna'
+}]
+
 export default function Pizza()
 {
     return (
