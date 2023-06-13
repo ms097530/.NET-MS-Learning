16d9653c97c26f096cc1f300a71ead1c881e68a4
Michael Schultz <mikewschultz9029@gmail.com>
2023-06-12 21:59:11 -0400

    added styling

--- Pizza_modules/pizza-frontend/src/Pizza.jsx
+++ Pizza_modules/pizza-frontend/src/Pizza.jsx
@@ -1,4 +1,34 @@
 import React, { useState } from 'react'
+import styled from 'styled-components'
+
+const PizzaFrame = styled.div`
+    border: solid 1px gray;
+    padding: 10px;
+    margin: 15px 10px;
+    border-radius: 5px;
+    box-shadow 0 0 5px gray;
+    font-family: Arial;
+`
+
+const Input = styled.input`
+    border: solid 1px black;
+    padding: 5px;
+    border-radius: 3px;
+`
+
+const Title = styled(Input)`
+    text-transform: uppercase;
+`
+
+const Save = styled.button`
+    width: 100px;
+    margin: 10px;
+    background: green;
+    color: white;
+    font-size: 16px;
+    padding: 10px;
+    border-radius: 5px;
+`
 
 export default function Pizza({ pizza })
 {
@@ -18,17 +48,17 @@ export default function Pizza({ pizza })
     }
 
     return (
-        <div>
+        <PizzaFrame>
             <h3>
-                <input onChange={(e) => update(e.target.value, 'name', data)} value={data.name} />
+                <Title onChange={(e) => update(e.target.value, 'name', data)} value={data.name} />
             </h3>
             <div>
-                <input onChange={(e) => update(e.target.value, 'description', data)} value={data.description} />
+                <Input onChange={(e) => update(e.target.value, 'description', data)} value={data.description} />
             </div>
             {
-                dirty ? <div><button onClick={onSave}>Save</button></div>
+                dirty ? <div><Save onClick={onSave}>Save</Save></div>
                     : null
             }
-        </div>
+        </PizzaFrame>
     )
 }
