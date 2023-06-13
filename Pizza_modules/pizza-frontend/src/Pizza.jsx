188006e27860ab41e36f8d984612005bdbff70d4
Michael Schultz <mikewschultz9029@gmail.com>
2023-06-12 21:38:04 -0400

    setup pizza component

--- Pizza_modules/pizza-frontend/src/Pizza.jsx
+++ Pizza_modules/pizza-frontend/src/Pizza.jsx
@@ -18,11 +18,24 @@ export default function Pizza({ pizza })
         setDirty(true)
     }
 
+    const onSave = () =>
+    {
+        setDirty(false)
+        // make rest call
+    }
+
     return (
         <div>
             <h3>
                 <input onChange={(e) => update(e.target.value, 'name', data)} value={data.name} />
             </h3>
+            <div>
+                <input onChange={(e) => update(e.target.value, 'description', data)} value={data.description} />
+            </div>
+            {
+                dirty ? <div><button onClick={onSave}>Save</button></div>
+                    : null
+            }
         </div>
     )
 }
