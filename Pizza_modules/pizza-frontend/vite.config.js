8188a129567ecaf05561e2b60659caf4fd0c585c
Michael Schultz <mikewschultz9029@gmail.com>
2023-06-14 17:21:02 -0400

    configured vite proxy

--- Pizza_modules/pizza-frontend/vite.config.js
+++ Pizza_modules/pizza-frontend/vite.config.js
@@ -4,4 +4,26 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
+      '^/fallback/.*': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/fallback/, '')
+      },
+      // * Using the proxy instance
+      // '/api': {
+      //   target: 'http://localhost:3000',
+      //   changeOrigin: true,
+      //   configure: (proxy, options) => {
+      //     // proxy will be an instance of 'http-proxy'
+      //   }
+      // }
+    }
+  }
 })
