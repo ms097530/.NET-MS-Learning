c191a6500388293f13bf8c4556d3eb5ded3e98a5
Michael Schultz <mikewschultz9029@gmail.com>
2023-06-14 17:25:35 -0400

    added test of proxy

--- Pizza_modules/pizza-frontend/src/App.jsx
+++ Pizza_modules/pizza-frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Pizza from './Pizza'
 import pizzas from './assets/pizzas'
@@ -6,6 +6,17 @@ import pizzas from './assets/pizzas'
 function App()
 {
   const data = pizzas.map(pizza => <Pizza key={pizza.id} pizza={pizza} />)
+
+  useEffect(() =>
+  {
+    async function getPizzas()
+    {
+      const res = await fetch('/api/pizzas')
+      const pizzas = await res.json()
+      console.log(pizzas)
+    }
+    getPizzas()
+  })
 
   return (
     <>
