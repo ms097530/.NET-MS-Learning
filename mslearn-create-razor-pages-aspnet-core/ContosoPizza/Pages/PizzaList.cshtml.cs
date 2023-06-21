1d5d85540ff739dc4e6f9c13f13e28696eeea2ff
Michael Schultz <mikewschultz9029@gmail.com>
2023-06-20 20:10:43 -0400

    setup PizzaList display

--- mslearn-create-razor-pages-aspnet-core/ContosoPizza/Pages/PizzaList.cshtml.cs
+++ mslearn-create-razor-pages-aspnet-core/ContosoPizza/Pages/PizzaList.cshtml.cs
@@ -1,12 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ContosoPizza.Models;
+using ContosoPizza.Services;
 
 namespace ContosoPizza.Pages
 {
     public class PizzaListModel : PageModel
     {
+        // * holds reference to a PizzaService object - can't be changed after set in ctor
+        private readonly PizzaService _service;
+        // * will hold a list of Pizza objects
+        // * initialized to default! to indicate to compiler that it will be initialized later, so null safety checks aren't required
+        public IList<Pizza> PizzaList { get; set; } = default!;
+
+        // * PizzaService object is provided by dependency injection (registered in Program.cs via builder.services.AddScoped<PizzaService>())
+        public PizzaListModel(PizzaService service)
+        {
+            _service = service;
+        }
+        // * retrieves list of pizzas from PizzaService object and stores it
         public void OnGet()
         {
+            PizzaList = _service.GetPizzas();
         }
     }
 }
