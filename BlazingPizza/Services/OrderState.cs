6c1048983a73104f65c243d66c096f014867df80
Michael Schultz <mikewschultz9029@gmail.com>
2023-07-15 11:49:27 -0400

    before refactoring pizza dialog to state

---
+++ BlazingPizza/Services/OrderState.cs
@@ -0,0 +1,34 @@
+namespace BlazingPizza.Services;
+
+public class OrderState
+{
+    public bool ShowingConfigureDialog { get; private set; }
+    public Pizza ConfiguringPizza { get; private set; }
+    public Order Order { get; private set; } = new Order();
+
+    public void ShowingConfigurePizzaDialog(PizzaSpecial special)
+    {
+        ConfiguringPizza = new Pizza()
+        {
+            Special = special,
+            SpecialId = special.Id,
+            Size = Pizza.DefaultSize,
+            Toppings = new List<PizzaTopping>()
+        };
+
+        ShowingConfigureDialog = true;
+    }
+
+    public void CancelConfigurePizzaDialog()
+    {
+        ConfiguringPizza = null;
+        ShowingConfigureDialog = false;
+    }
+
+    public void ConfirmConfigurePizzaDialog()
+    {
+        Order.Pizzas.Add(ConfiguringPizza);
+        ConfiguringPizza = null;
+        ShowingConfigureDialog = false;
+    }
+}
